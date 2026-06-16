0020762088a3b4d5455f54ed08f61c6bef5c0faa
Paiva <danpaiva@live.com>
2026-06-15 21:03:08 -0300

    feat: add seo map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://danhpaiva.github.io',
   base: '/',
   output: 'static',
+  integrations: [sitemap()],
 });
